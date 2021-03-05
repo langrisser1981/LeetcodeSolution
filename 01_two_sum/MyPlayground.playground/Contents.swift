28217451bea64f23b913dee1a27b0facb0d502af
lenny_chenh <langrisser1981@gmail.com>
2021-03-05 16:47:15 +0800

    第一題two_sum解完 利用dict模擬hashmap，在dict裡面尋找目標值，如果不存在就將值存入map

--- 01_two_sum/MyPlayground.playground/Contents.swift
+++ 01_two_sum/MyPlayground.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
 var str = "two_sum"
+class Solution{
+    func twoSum(_ nums:[Int], _ target:Int) -> [Int]{
+        var numberIndexDict = [Int:Int]()
+        
+        for (index, num) in nums.enumerated(){
+            guard let pairedIndex = numberIndexDict[target - num] else{
+                numberIndexDict[num] = index
+                continue
+            }
+            
+            return [pairedIndex, index]
+        }
+        
+        return [-1,-1]
+    }
+}
+
+let solution = Solution()
+solution.twoSum([3,2,4], 5)
